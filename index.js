f851f612f807e0b0a3658c91531f0982a885f3ab
pratiktiwari14 <kumarpratiek@gmail.com>
2021-09-30 20:54:09 +0530

    Dice Game

---
+++ index.js
@@ -0,0 +1,20 @@
+var randomNumber1= Math.floor(Math.random()*6)+1;
+var randomDiceImage= "d"+randomNumber1+".png";
+var randomImagesSource= "images/"+ randomDiceImage;
+var image1= document.querySelectorAll("img")[0];
+image1.setAttribute("src" , randomImagesSource);
+
+var randomNumber2= Math.floor(Math.random()*6)+1;
+randomImageSource2= "images/d"+ randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src" , randomImageSource2);
+
+
+if(randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML= "Player 1 wins";
+}
+else if(randomNumber1 < randomNumber2){
+  document.querySelector("h1").innerHTML= "Player 2 wins";
+}
+else{
+  document.querySelector("h1").innerHTML="Draw!!";
+}
