ba28361b8b5047d51881d79bcc7aa68a8ec361d4
pratiktiwari14 <kumarpratiek@gmail.com>
2021-09-30 21:26:49 +0530

    images changed

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var randomNumber1= Math.floor(Math.random()*6)+1;
-var randomDiceImage= "d"+randomNumber1+".png";
+var randomDiceImage= "dice"+randomNumber1+".png";
 var randomImagesSource= "images/"+ randomDiceImage;
 var image1= document.querySelectorAll("img")[0];
 image1.setAttribute("src" , randomImagesSource);
 
 var randomNumber2= Math.floor(Math.random()*6)+1;
-randomImageSource2= "images/d"+ randomNumber2 + ".png";
+randomImageSource2= "images/dice"+ randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src" , randomImageSource2);
 
 
